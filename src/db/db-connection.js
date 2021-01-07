8cc9062e2bcc50f4959e06a4a7e913a281385c37
Orhan Taghizadeh <43483015+tagizadeorxan@users.noreply.github.com>
2021-01-08 01:24:04 +0400

    db_port add

--- src/db/db-connection.js
+++ src/db/db-connection.js
@@ -8,7 +8,8 @@ class DBConnection {
             host: process.env.DB_HOST,
             user: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE
+            database: process.env.DB_DATABASE,
+            port:process.env.DB_PORT
         });
 
         this.checkConnection();
